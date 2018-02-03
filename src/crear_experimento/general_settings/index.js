53c7e8c0229b5377a71a3121bcd8b9fb77270418
pablobovina <pablobovina@gmail.com>
2018-02-03 10:43:11 -0300

    agrego campos name y description a general settings

--- src/crear_experimento/general_settings/index.js
+++ src/crear_experimento/general_settings/index.js
@@ -7,21 +7,36 @@ class GeneralSettings extends Component {
     this.state = {};
   }
 
+  saveInput = (e) =>{
+    const id =  e.target.getAttribute("id");
+    const v = e.target.value;
+    this.setState((prevState)=>{
+      var r = {};
+      r[id] = v;
+      return r;
+    });
+    this.props.saveAction(this.state);
+  }
+
   render() {
     const res = <form id="acquire">
     <h2> General Settings </h2>
+    <label for="a_name">Name</label>
+    <input type="text" class="form-control" id="a_name" placeholder="Name" onChange={this.saveInput}/>
+    <label for="a_description" required>Description</label>
+    <textarea class="form-control" id="a_description" placeholder="Description" onChange={this.saveInput}/>
     <label for="a_times">Times</label>
-    <input type="text" class="form-control" id="a_times" placeholder="Times"/>
+    <input type="text" class="form-control" id="a_times" placeholder="Times" onChange={this.saveInput}/>
     <label for="a_bloq">Blocks</label>
-    <input type="text" class="form-control" id="a_bloq" placeholder="Blocks"/>
+    <input type="text" class="form-control" id="a_bloq" placeholder="Blocks" onChange={this.saveInput}/>
     <label for="a_time">Time</label>
-    <input type="text" class="form-control" id="a_time" placeholder="Time"/>
+    <input type="text" class="form-control" id="a_time" placeholder="Time" onChange={this.saveInput}/>
     <label for="a_freq">Frequency</label>
-    <input type="text" class="form-control" id="a_freq" placeholder="Freq"/>
+    <input type="text" class="form-control" id="a_freq" placeholder="Freq" onChange={this.saveInput}/>
     <label for="a_lsb">LSB</label>
-    <input type="text" class="form-control" id="a_lsb" placeholder="LSB 8-bit"/>
+    <input type="text" class="form-control" id="a_lsb" placeholder="LSB 8-bit" onChange={this.saveInput}/>
     <label for="a_msb">MSB</label>
-    <input type="text" class="form-control" id="a_msb" placeholder="MSB 8-bit"/>
+    <input type="text" class="form-control" id="a_msb" placeholder="MSB 8-bit" onChange={this.saveInput}/>
     </form>;
     return res;
   }
