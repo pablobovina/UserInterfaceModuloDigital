0a40c7c46b14d2d6cd5558bafb67e16acd0879d1
pablobovina <pablobovina@gmail.com>
2018-12-26 06:43:22 -0300

    fix menu de acciones

--- src/lista_de_experimentos/action_panel/index.js
+++ src/lista_de_experimentos/action_panel/index.js
@@ -116,10 +116,10 @@ class ActionPanel extends Component {
 
     const res =
     <nav className="navbar navbar-expand-lg navbar-light">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent1">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
